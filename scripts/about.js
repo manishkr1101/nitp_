e6a47abcaa0d0fa3a1a41356f64348564c1c3f33
Manish Kumar <manishkumarsh120@gmail.com>
2019-07-02 20:57:18 +0530

    iframe issue resolved

--- scripts/about.js
+++ scripts/about.js
@@ -7,6 +7,9 @@ window.onload = function(){
 
 function press(id){
 	$('iframe').attr('src', "iframe/about/"+id+".html")
+	// changeHeight(document.querySelector('iframe'));
+	//for dynamic size of sidebar
+	// $('.menu').height($('.content').height() > $('.menu ul').height() ? $('.content').height() : $('.menu ul').height() + $('.menu > div').height() +15 )
 	$('.menu li').removeClass('clicked');
 	$("#"+id).addClass('clicked');
 	$('.content h1').html($("#"+id).html());
@@ -22,3 +25,9 @@ $('.menu .expnd-btn img').click(function(){
 	$('.menu ul').slideToggle();
 });
 
+
+// changing height of iframe to its content
+function changeHeight(iframe){
+	iframe.height = 0;//it seems like foolish but very necessary
+	iframe.height = iframe.contentWindow.document.body.scrollHeight + 10;
+}
